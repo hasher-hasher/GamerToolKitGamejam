349b4dae61d3fc7307b60845a4090058f7e58812
Renan Rocha <legend.oliveira@gmail.com>
2019-08-03 12:55:00 +0200

    Adding bird and saw

---
+++ Assets/Scripts/EnemyBase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public abstract class EnemyBase : MonoBehaviour
+{
+    public enum Enemy
+    {
+        Spin,
+        Go_Left
+    }
+
+    public Enemy enemyType;
+
+    public float Speed;
+
+    public virtual void FixedUpdate() {
+        switch (enemyType) {
+            case Enemy.Spin:
+                transform.Rotate(0f, 0f, Time.deltaTime * Speed);
+                break;
+            case Enemy.Go_Left:
+                GetComponent<Rigidbody2D>().velocity = Vector2.left * Speed * Time.deltaTime;
+                break;
+        }
+    }
+}
