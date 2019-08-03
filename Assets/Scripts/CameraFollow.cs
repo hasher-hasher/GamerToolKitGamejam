1cd17536247e596710b45a9456fc2f6ef1c2b660
Renan Rocha <legend.oliveira@gmail.com>
2019-08-03 12:13:00 +0200

    Appying camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Transform player;
+    Vector3 desiredPosition;
+
+    private void Start() {
+        player = GameObject.Find("Player").transform;
+    }
+    private void FixedUpdate() {
+        desiredPosition = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        if (player.transform.position.x >= transform.position.x) {
+            transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 10f);
+        }
+    }
+}
